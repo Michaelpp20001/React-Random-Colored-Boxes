bd7fc9bd04f9cc9be8675c5b3e86656ec92f9fed
Michael <michaelpp2001@gmail.com>
2020-04-21 17:28:13 -0700

    boiler plate for exercise

---
+++ src/randomBoxes.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './randomBoxes.css';
+
+class RandomBoxes extends Component {
+
+    render () {
+        return (
+            <div className="App">
+                Hello boxes!
+            </div>
+        );
+    }
+}
+
+export default RandomBoxes;
